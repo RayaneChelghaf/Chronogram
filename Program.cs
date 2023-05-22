faa8da8ec005473c83a961b8abfab15580a173e6
2be-Rayane <125278584+2be-Rayane@users.noreply.github.com>
2023-05-22 16:50:23 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using NAudio.Wave;
+
+Console.WriteLine("Hello, World!");
+
+var fileStream = File.Open(@"C:\Users\Rayane\Desktop\dépot temp\Chronogramme\test.wav", FileMode.Open);
+
+WaveFileReader reader = new WaveFileReader(fileStream) ;
+
+int count = 0;
+
+float[] lastValue = null; 
+
+while (true)
+{
+    var res = reader.ReadNextSampleFrame();
+
+    if (count++ % 1000 == 0)
+        Console.WriteLine(count);
+
+    if (res != null)
+        lastValue = res; 
+
+    if (res == null)
+        break; 
+}
+
+
+
+
+
+
+Console.WriteLine() ;
---
+++ README.md
@@ -0,0 +1,9 @@
+Ce code utilise la bibliothèque NAudio pour lire un fichier audio WAV et afficher le nombre d'échantillons lus.
+
+## Utilisation
+
+
+Inclure la référence à la bibliothèque NAudio.Wave.
+
+
+Exécuter le code.
